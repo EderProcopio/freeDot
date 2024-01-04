5ffed1470d8f70d944262fb2adafb87e0e8a601f
EderProcopio <dinho15email@gmail.com>
2024-01-04 14:29:24 -0300

    Exclusão de linhas vazias.

--- index.js
+++ index.js
@@ -6,26 +6,73 @@ const ctx = canvas.getContext("2d");
 let x = 100;
 let y = 100;
 let radius = 50;
+let speed = 10;
 
+let upPressed = false;
 let downPressed = false;
+let leftPressed = false;
+let rightPressed = false;
+
 
 
 //Loop do jogo
 function drawGame() {
-    requestAnimationFrame(drawGame);
     clearScreen();
     inputs();
+    boundryCheck();
     drawPinkCircle();
+    requestAnimationFrame(drawGame);
+}
+
+function boundryCheck() {
+    //cima
+    if (y < radius) {
+        y = radius;
+    }
+   
+    //baixo
+    if (y > canvas.height - radius) {
+        y = canvas.height - radius;
+    }
+
+    //esquerda
+    if (x < radius) {
+        x = radius;
+    }
+
+    //direita
+    if (x > canvas.width - radius) {
+        x = canvas.width - radius;
+    }
 }
 
 function inputs() {
-    if(downPressed) {
-        y = y + 10;
+    if (upPressed) {
+        y = y - speed;
+    }
+    if (downPressed) {
+        y = y + speed;
+    }
+    if (leftPressed) {
+        x = x - speed;
+    }
+    if (rightPressed) {
+        x = x + speed;
     }
 }
 
 function drawPinkCircle() {
-    ctx.fillStyle = "pink";
+    ctx.fillStyle = "#ffbbdb";
+
+    if (upPressed || downPressed || leftPressed || rightPressed) {
+        ctx.fillStyle = "#ff8fdb";
+    }
+
+    // Adiciona verificação para verificar se o círculo atingiu as margens
+    if (y === radius || y === canvas.height - radius || x === radius || x === canvas.width - radius) {
+        ctx.fillStyle = "#ff0000";  // Define a cor como vermelha se atingir as margens
+    }
+
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, Math.PI * 2);
     ctx.fill();
@@ -37,19 +84,51 @@ function clearScreen() {
     ctx.fillRect(0,0, canvas.width, canvas.height);
 }
 
-document.body.addEventListener("keydown", keyDown);
-document.body.addEventListener("keyup", keyUp);
+document.body.addEventListener("keydown", keyPressed);
+document.body.addEventListener("keyup", keyReleased);
 
-function keyDown(event) {
+function keyPressed(event) {
+    //cima
+    if (event.code === 'ArrowUp') {
+        upPressed = true;
+    }
+
     //baixo
-    if (event.keyCode == 40) {
+    if (event.code === 'ArrowDown') {
         downPressed = true;
+    }
+
+    //esquerda
+    if (event.code === 'ArrowLeft') {
+        leftPressed = true;
+    }
+
+    //direita
+    if (event.code === 'ArrowRight') {
+        rightPressed = true;
     }
 };
 
-function keyUp(event) {
-    if (event.keyCode == 40) {
+function keyReleased(event) {
+    //cima
+    if (event.code === 'ArrowUp') {
+        upPressed = false;
+    }
+
+    //baixo
+    if (event.code === 'ArrowDown') {
         downPressed = false;
     }
+
+    //esquerda
+    if (event.code === 'ArrowLeft') {
+        leftPressed = false;
+    }
+
+    //direita
+    if (event.code === 'ArrowRight') {
+        rightPressed = false;
+    }
+
 };
 drawGame();
